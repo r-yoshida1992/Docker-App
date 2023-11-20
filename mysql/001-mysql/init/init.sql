81458e4c10453c850a82ad098c52c308d1e7ca6a
Ren Yoshida <ren.yoshida1992@gmail.com>
2023-11-21 07:10:21 +0900

    files move and add mysql.

---
+++ mysql/001-mysql/init/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    age INT
+);
+INSERT INTO customers (name, email, age)
+VALUES ('John Doe', 'johndoe@example.com', 30);
+```
